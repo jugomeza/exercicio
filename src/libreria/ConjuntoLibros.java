f0724234c921f5727c1e2795aa7cb501f4b50170
Estudiante <Estudiante@10.203.1.89>
2018-09-05 08:22:59 -0500

    Trabajo clase

---
+++ src/libreria/ConjuntoLibros.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreria;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class ConjuntoLibros {
+ 
+    public String[] añadirs(){
+        System.out.println("ingrese el numero de arreglos que desea crear");
+        
+    String cadena[]= new String[];}
+    
+    public boolean ellt(String a){
+    }
+ public boolean ella(String a){
+ }
+ public Libro(){}
+ public Libro getbmnc(){}
+ public void getLibros(String[]){}
+ 
+    
+}
